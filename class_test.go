ee5b38160e68b2858f92227104b33e6b111b586d
mschulte <mschulte@eogresources.com>
2020-05-09 22:15:25 -0500

    added tests for unreachable and other dtypes in ToClass

--- class_test.go
+++ class_test.go
@@ -4,35 +4,42 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gorgonia.org/tensor"
+	T "gorgonia.org/tensor"
 )
 
 func TestToClass(t *testing.T) {
 	// TODO: panic without an iterator
 	// Test panic on non vector
-	panicOnNonVector := func() {
-		_ = ToClass(tensor.New(tensor.Of(tensor.Float32), tensor.WithShape(10, 10)), 0)
-	}
-	assert.Panics(t, panicOnNonVector)
-	// Panic on unsupported tensor dtype
-	panicOnUnsupportedType := func() {
-		_ = ToClass(tensor.New(tensor.Of(tensor.Complex64), tensor.WithShape(10)), 0)
-	}
-	assert.Panics(t, panicOnUnsupportedType)
+	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Float32), T.WithShape(10, 10)), 0) })
+	// Panic on unsupported T dtype
+	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Complex64), T.WithShape(10)), 0) })
+	// Panic on Unreachable
+	assert.Panics(t, func() { ToClass(T.New(T.WithBacking([]float32{0, 0, 1, 0, 0})), 999) })
 
-	// // Float32
-	// dataFloat32 := []float32{0, 0, 1, 0, 0}
-	// resultFloat32 := ToClass(tensor.New(tensor.WithBacking(dataFloat32)), 0)
-	// assert.Equal(t, resultFloat32, Class(2))
-
-	// dataFloat64 := []float64{0, 1, 2, 3, 4}
-	// t.Log(ToClass(tensor.New(tensor.WithBacking(dataFloat64)), 0))
-
-	// dataInt := []int{0, 1, 2, 3, 4}
-	// t.Log(ToClass(tensor.New(tensor.WithBacking(dataInt)), 0))
-
-	// dataUint := []uint{0, 1, 2, 3, 4}
-	// t.Log(ToClass(tensor.New(tensor.WithBacking(dataUint)), 0))
+	// From here On only specifying vectors of length 5 varying type and threshold
+	// Float32
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{0, 0, 1, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{0.1, 0.1, 0.6, 0.7, 0.1})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{0.1, 0.1, 0.6, 0.7, 0.1})), 0.65), Class(3))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{1, 1, 1, 1, 1})), 0), Class(0))
+	// Float64
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float64{0, 0, 1, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float64{0.1, 0.1, 0.6, 0.7, 0.1})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float64{0.1, 0.1, 0.6, 0.7, 0.1})), 0.65), Class(3))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]float64{1, 1, 1, 1, 1})), 0), Class(0))
+	// Int
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{0, 0, 1, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{0, 0, 1, 0, 0})), 999), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{0, 0, 2, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{0, 1, 2, 0, 0})), 0), Class(1))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{1, 1, 1, 1, 1})), 0), Class(0))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]int{-1, -2, -3, 0, 1})), 0), Class(4))
+	// uint
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]uint{0, 0, 1, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]uint{0, 0, 1, 0, 0})), 999), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]uint{0, 0, 2, 0, 0})), 0), Class(2))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]uint{0, 1, 2, 0, 0})), 0), Class(1))
+	assert.Equal(t, ToClass(T.New(T.WithBacking([]uint{1, 1, 1, 1, 1})), 0), Class(0))
 }
 
 func TestToClasses(t *testing.T) {
