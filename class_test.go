46a7c1e9ab5e8829b69e35f88a90b6f2f558ec00
mschulte <mschulte@eogresources.com>
2020-05-09 23:06:43 -0500

    last commit was accidentally testing the wrong function also have basic test case with all dtypes

--- class_test.go
+++ class_test.go
@@ -4,10 +4,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gorgonia.org/tensor"
 	T "gorgonia.org/tensor"
 )
 
 func TestToClass(t *testing.T) {
+	// Panic Tests
 	// TODO: panic without an iterator
 	// Test panic on non vector
 	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Float32), T.WithShape(10, 10)), 0) })
@@ -15,8 +17,8 @@ func TestToClass(t *testing.T) {
 	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Complex64), T.WithShape(10)), 0) })
 	// Panic on Unreachable
 	assert.Panics(t, func() { ToClass(T.New(T.WithBacking([]float32{0, 0, 1, 0, 0})), 999) })
-
-	// From here on only specifying vectors of length 5 varying type and threshold
+	// Value Tests
+	// only specifying vectors of length 5 varying values, type, and threshold
 	// Float32
 	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{0, 0, 1, 0, 0})), 0), Class(2))
 	assert.Equal(t, ToClass(T.New(T.WithBacking([]float32{0.1, 0.1, 0.6, 0.7, 0.1})), 0), Class(2))
@@ -43,14 +45,33 @@ func TestToClass(t *testing.T) {
 }
 
 func TestToClasses(t *testing.T) {
+	// Panic Tests
 	// TODO: panic without an iterator
 	// Test panic on non matrix
-	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Float32), T.WithShape(10)), 0) })
-	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Float32), T.WithShape(10, 10, 10)), 0) })
+	assert.Panics(t, func() { ToClasses(T.New(T.Of(T.Float32), T.WithShape(10)), 0) })
+	assert.Panics(t, func() { ToClasses(T.New(T.Of(T.Float32), T.WithShape(10, 10, 10)), 0) })
 	// Panic on unsupported T dtype
-	assert.Panics(t, func() { ToClass(T.New(T.Of(T.Complex64), T.WithShape(10)), 0) })
+	assert.Panics(t, func() { ToClasses(T.New(T.Of(T.Complex64), T.WithShape(10)), 0) })
 	// Panic on Unreachable
-	assert.Panics(t, func() { ToClass(T.New(T.WithBacking([][]float32{{0, 0, 1}, {0, 0, 1}})), 999) })
+	assert.Panics(t, func() { ToClasses(T.New(T.WithBacking([][]float32{{0, 0, 1}, {0, 0, 1}})), 999) })
+	// Value Tests
+	// only specifying matracies of length 2x3 varying values, type, and threshold
+	var matF32, matF64, matInt, matUint tensor.Tensor
+	// Float32
+	// []float32{0.1, 0.6, 0.7, 0.1, 0.7, 0.6}
+	// []float32{0.1, 0.6, 0.7, -1.0, 0.2, 0.6}
+	// []float32{0.1, 0.6, 0.7, -1.0, 0.2, 0.6}
+	matF32 = T.New(T.WithBacking([]float32{0, 0, 1, 0, 1, 0}), T.WithShape(2, 3))
+	assert.Equal(t, ToClasses(matF32, 0), []Class{2, 1})
+	// Float64
+	matF64 = T.New(T.WithBacking([]float64{0, 0, 1, 0, 1, 0}), T.WithShape(2, 3))
+	assert.Equal(t, ToClasses(matF64, 0), []Class{2, 1})
+	// Int
+	matInt = T.New(T.WithBacking([]int{0, 0, 1, 0, 1, 0}), T.WithShape(2, 3))
+	assert.Equal(t, ToClasses(matInt, 0), []Class{2, 1})
+	// Uint
+	matUint = T.New(T.WithBacking([]uint{0, 0, 1, 0, 1, 0}), T.WithShape(2, 3))
+	assert.Equal(t, ToClasses(matUint, 0), []Class{2, 1})
 }
 
 func TestToOneHotVector(t *testing.T) {
